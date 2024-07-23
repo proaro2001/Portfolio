8180e14558c3f92a1aef449c707427675421bb88
proaro2001 <proaro2001@gmail.com>
2024-07-23 16:23:33 -0700

    Updates images path

--- src/components/Projects/Project_List.jsx
+++ src/components/Projects/Project_List.jsx
@@ -1,24 +1,24 @@
 const project_list = [
   {
-    image: <img src="angry_bird_3d.png" />,
+    image: <img src="/angry_bird_3d.png" />,
     title: "Angry Bird 3D",
     demo_link: "https://parthgu.github.io/AngryBirdsFinalDemo/",
     github_link: "",
   },
   {
-    image: <img src="Leetcode-extractor-demo.png" />,
+    image: <img src="/Leetcode-extractor-demo.png" />,
     title: "Leetcode Extractor",
     demo_link: "",
     github_link: "https://github.com/proaro2001/Leetcode-Problem-Extractor",
   },
   {
-    image: <img src="emotion_detection_test_random.png" />,
+    image: <img src="/emotion_detection_test_random.png" />,
     title: "Emotion Classification",
     demo_link: "",
     github_link: "",
   },
   {
-    image: <img src="chat_app_demo.png" />,
+    image: <img src="/chat_app_demo.png" />,
     title: "CHAT-APP",
     demo_link: "https://chat-app-2y99.onrender.com",
     github_link: "",
